0eaf849a72add9598dcc5320e6253c2581a04229
Sertaç Onay <sertac@onay.me>
2017-08-31 13:08:23 +0300

    contains constants

---
+++ core/constants.js
@@ -0,0 +1,7 @@
+export const relatedTags = 'https://www.instagram.com/web/search/topsearch/?context=blended&query=snow';
+export const instaQuery = 'https://www.instagram.com/graphql/query/?query_id=17875800862117404&variables=%7B%22tag_name%22%3A%22snow%22%2C%22first%22%3A8%2C%22after%22%3A%22J0HWaYL6QAAAF0HWaYLRQAAAFiYA%22%7D';
+
+export const instagramAPI = 'https://api.instagram.com/v1';
+export const userID = '5955180865';
+export const clientID = 'e097439da7034238a474a2a1e30b404b';
+export const accessToken = '470934347.e097439.78e94c9865134e30893011363d61a895';
