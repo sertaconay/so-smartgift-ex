421bb65810c1b77d19cb2a55d0790f6503d0a138
Sertaç Onay <sertac@onay.me>
2017-08-31 13:08:06 +0300

    initializing state

---
+++ core/initialStore.js
@@ -0,0 +1,10 @@
+const initialState = {
+  media: {}, // for single media
+  userMedia: {}, // for all media of a user
+  tag: {}, // for single tag
+  tagMedia: {}, // for all media of a tag
+  tagSearch: {}, // for related tags
+  previousTags: [], // for previously search tags
+};
+
+export default initialState;
