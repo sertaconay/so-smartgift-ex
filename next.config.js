e9781a8c6504e9d817c883ab0a0cc1c2746c4131
Sertaç Onay <sertac@onay.me>
2017-08-31 13:06:25 +0300

    created next config

---
+++ next.config.js
@@ -0,0 +1,22 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { ANALYZE } = process.env;
+
+module.exports = {
+  webpack: function (config, { dev }) {
+    if (ANALYZE) {
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'server',
+        analyzerPort: 8888,
+        openAnalyzer: true,
+      }));
+    }
+    if (dev) {
+      config.module.rules.push({
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      });
+    }
+    return config;
+  }
+};
