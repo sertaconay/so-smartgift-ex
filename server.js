d415cb1a0a608d044aaf3fd52296e5919af0fe74
Sertaç Onay <sertac@onay.me>
2017-09-01 15:28:12 +0300

    get/post settings

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const next = require('next');
-const routes = require('./routes');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const handler = routes.getRequestHandler(app);
-
 
 app
   .prepare()
   .then(() => {
     const server = express();
-    server.use(handler);
 
-    // server.get('/tag/:slug', (req, res) => {
-    //   const actualPage = '/tag'
-    //   const queryParams = { tag: req.params.slug }
-    //   app.render(req, res, actualPage, queryParams)
-    // })
+    server.all('/tag/:slug', (req, res) => {
+      const actualPage = '/tag';
+      const queryParams = { tag: req.params.slug };
+      app.render(req, res, actualPage, queryParams);
+    });
 
     server.get('*', (req, res) => {
       return handle(req, res);
